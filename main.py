874586e5108aa3d02d0438d5867ef53b38a77ae5
Gopal Patil <gayatri.gopal-patil@capgemini.com>
2025-12-01 13:30:49 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+
+from fastapi import FastAPI
+import os
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"Hello": "This is home page"}
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
